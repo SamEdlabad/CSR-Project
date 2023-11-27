26a3b42a017d0266ecf078299dda116c8bde0d69
SamEdlabad <93589971+SamEdlabad@users.noreply.github.com>
2023-11-27 09:42:42 -0500

    Update settings.py

--- csrproject/settings.py
+++ csrproject/settings.py
@@ -142,4 +142,4 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_SSL = True
 EMAIL_PORT = 465
 EMAIL_HOST_USER = 'csrplatform23@gmail.com'
-EMAIL_HOST_PASSWORD = 'fdueahxcxhrntbem'
+EMAIL_HOST_PASSWORD = '****************'
